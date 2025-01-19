da0509e8388cf0060b969b8ebb37d46bef94b66a
Muhannadh <61771295+mrmuhannadh@users.noreply.github.com>
2025-01-19 20:58:58 +0530

    build error fix

--- utils/supabase/model.ts
+++ utils/supabase/model.ts
@@ -1,5 +1,6 @@
 import { Database } from "./database";
 
-export type IRegistedUsers = Database["public"]["Tables"]["resigtered_users"]["Row"];
+export type IRegistedUsers = Database["public"]["Tables"]["registered_user"]["Row"];
 export type IPlayerUserProfile = Database["public"]["Tables"]["player_user_profile"]["Row"];
 export type IAdminUserProfile = Database["public"]["Tables"]["admin_user_profile"]["Row"];
+export type INotifications = Database["public"]["Tables"]["notifications"]["Row"];
